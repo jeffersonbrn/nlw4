8069e34acca4d9a8759a95dd5c58abc11910849c
Bruno Reis <jeffersonbrn@live.com>
2021-03-03 13:29:01 -0300

    Adicionando novos itens.md

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,18 @@
-import express from 'express';
+import 'reflect-metadata'
+import express from "express";
+import "./database"
+
 const app = express();
 
-app.listen(3000, () => console.log("Server is runnung!"));
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World" });
+});
+
+app.post("/", (request, response) =>{
+    // recebeu os dados para salvar
+    return response.json({message: "Os dados foram salvos com sucesso"});
+});
+
+app.listen(3333, () => console.log("Server is runnung!"));
+
